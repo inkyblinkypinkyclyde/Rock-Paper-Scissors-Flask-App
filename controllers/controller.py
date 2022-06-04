4430dcda2c4976b3379e22b7214c75d55aae1e3f
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-06-04 22:49:13 +0100

    pvpdropdown, pvedropdown and pvpurl all working here

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,8 +3,20 @@ from app import app
 from models.players import *
 from models.player import *
 
+@app.route("/rps")
+def home_page():
+    return render_template('home.html', title='Welcome to RPS')
 
-#pve routes
+##### This works, don't touch this
+@app.route('/rps/<player1_choice>/<player2_choice>')
+def take_choices(player1_choice, player2_choice):
+    player1 = Player("John", player1_choice)
+    player2 = Player("Jarrod", player2_choice)
+    return render_template('pvp_URL_rps.html', title= 'PvP URL entry mode', result = play_game_URL_input(player1, player2))
+##### This works, don't touch this
+
+
+##### This works, don't touch this
 @app.route("/rps/pve")
 def index_pve():
     return render_template('pve_rps.html', title= 'PvE mode',)
@@ -13,30 +25,28 @@ def index_pve():
 def pve_play():
     outcome = play_game_pve(request.form['option'])
     return render_template('pve_rps.html', title= 'PvE mode', outcome = outcome)
+##### This works, don't touch this
 
-# #pvp routes  I'll fix these later
-# @app.route("/rps/pvp")
-# def pvp_play():
-#     return render_template('pvp_rps.html', title= 'PvP mode',)
 
-# @app.route("/rps/pvp", methods=['post'])
-# def pvp_play():
-#     outcome = play_game_pve(request.form['option'])
-#     return render_template('pve_rps.html', title= 'PvE mode', outcome = outcome)
-
-@app.route("/rps")
-def index_pvp():
+##### This works, don't touch this
+@app.route("/rps/pvp")
+def pvp_play():
     return render_template('pvp_rps.html', title= 'PvP mode',)
 
+@app.route("/rps/pvp", methods=['post'])
+def pvp_play2():
+    outcome = play_game_select_input(request.form['player1_choice'], request.form['player2_choice'])
+    return render_template('pvp_rps.html', title= 'PvP mode', outcome = outcome)
+##### This works, don't touch this
 
-@app.route('/rps/<player1_choice>/<player2_choice>')
-def take_choices(player1_choice, player2_choice):
-    player1 = Player("John", player1_choice)
-    player2 = Player("Jarrod", player2_choice)
-    return render_template('pvp_URL_rps.html', title= 'PvP URL entry mode', result = play_game_URL_input(player1, player2))
 
-#player navigates to rps/rock/paper
-#create player1 object w/name player 1 choice rock
-#create player2 object w/name player 2 choice paper
+@app.route("/rps/LAN/player1")
+def lan_player1():
+    return "player1 area"
+
+@app.route("/rps/LAN/player2")
+def lan_player2():
+    return "player2 area"
+
 
 
--- models/players.py
+++ models/players.py
@@ -31,42 +31,45 @@ def play_game_pve(choice):
             return "Scissors cut paper. You lose"
 
 def play_game_URL_input(player1, player2):
-        if player1.choice == player2.choice:
-            return "It's a draw, you chose the same thing."
-        elif player1.choice == "rock":
-            if player2.choice == "scissors":
-                return "Player one wins! Rock crushes scissors."
-            else: 
-                return "Player two wins! Paper wraps rock."
-        elif player1.choice == "scissors":
-            if player2.choice == "paper":
-                return "Player one wins! Scissors wrap paper."
-            else:
-                return "Player two wins! Rock crushes scissors."
-        elif player1.choice == "paper":
-            if player2.choice == "rock":
-                return "Player one wins! Paper wraps rock."
-            else:
-                return "Player two wins! Scissors cut paper."
-
-
-
+    if player1.choice == player2.choice:
+        return "It's a draw, you chose the same thing."
+    elif player1.choice == "rock":
+        if player2.choice == "scissors":
+            return "Player one wins! Rock crushes scissors."
+        else: 
+            return "Player two wins! Paper wraps rock."
+    elif player1.choice == "scissors":
+        if player2.choice == "paper":
+            return "Player one wins! Scissors wrap paper."
+        else:
+            return "Player two wins! Rock crushes scissors."
+    elif player1.choice == "paper":
+        if player2.choice == "rock":
+            return "Player one wins! Paper wraps rock."
+        else:
+            return "Player two wins! Scissors cut paper."
 
 def play_game_select_input(player1_choice, player2_choice):
-        if player1_choice == player2_choice:
-            return "It's a draw, you chose the same thing."
-        elif player1_choice == "rock":
-            if player2_choice == "scissors":
-                return "Player one wins! Rock crushes scissors."
-            else: 
-                return "Player two wins! Paper wraps rock."
-        elif player1_choice == "scissors":
-            if player2_choice == "paper":
-                return "Player one wins! Scissors wrap paper."
-            else:
-                return "Player two wins! Rock crushes scissors."
-        elif player1_choice == "paper":
-            if player2_choice == "rock":
-                return "Player one wins! Paper wraps rock."
-            else:
-                return "Player two wins! Scissors cut paper."
+    if player1_choice == player2_choice:
+        return "It's a draw, you chose the same thing."
+    elif player1_choice == "rock":
+        if player2_choice == "scissors":
+            return "Player one wins! Rock crushes scissors."
+        elif player2_choice == "paper": 
+            return "Player two wins! Paper wraps rock."
+        else:
+            return "something went wrong1. You shouldn't see this string"       
+    elif player1_choice == "scissors":
+        if player2_choice == "paper":
+            return "Player one wins! Scissors wrap paper."
+        elif player2_choice == "rock":
+            return "Player two wins! Rock crushes scissors."
+        else:
+            return "something went wrong2. You shouldn't see this string"
+    elif player1_choice == "paper":
+        if player2_choice == "rock":
+            return "Player one wins! Paper wraps rock."
+        elif player2_choice == "scissors":
+            return "Player two wins! Scissors cut paper."
+        else:
+            return "something went wrong3. You shouldn't see this string"
